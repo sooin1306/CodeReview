bc992c7ee6ad14f40b0f660f4171117aaf69f85d
youngyoung0 <kxodud10054@gmail.com>
2021-01-04 17:14:26 +0900

    Create StudentApplication.java

---
+++ homework/studentClass/StudentApplication.java
@@ -0,0 +1,32 @@
+package studentClass;
+
+import java.util.Scanner;
+
+public class StudentApplication {
+	public static void main(String[] args) {
+		System.out.println("---------------------------------------------------------");
+		System.out.println("1.반생성    2.학생추가    3.학생제거    4.학생수정    5.평균    6.합계    7.평균");
+		System.out.println("---------------------------------------------------------");
+		
+		Scanner sc = new Scanner(System.in);
+		String menu = sc.next();
+		
+		while (true) {
+			if(menu.equals("1")) {
+				
+			}else if(menu.equals("2")) {
+				
+			}else if(menu.equals("3")) {
+				
+			}else if(menu.equals("4")) {
+				
+			}else if(menu.equals("5")) {
+				
+			}else if(menu.equals("6")) {
+				
+			}else if(menu.equals("7")) {
+				
+			}
+		}
+	}
+}
